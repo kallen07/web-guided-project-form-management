1b8f644f067a336ff1fa7c99c89e6a7a59b1dfd3
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-26 13:01:17 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -36,7 +36,7 @@ export default function App() {
       role: formValues.role,
     };
     //  b) prevent further action if either username or email or role is empty string after trimming
-    debugger;
+    if (!newFriend.username || !newFriend.email || !newFriend.role) return;
     //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
     //  d) also on success clear the form
   };

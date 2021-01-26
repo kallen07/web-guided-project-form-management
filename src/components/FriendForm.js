ac38873948cf30dfdc0827d6828d2e9124c3baa5
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-26 12:21:12 -0600

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -28,6 +28,9 @@ export default function FriendForm(props) {
               Controlled inputs need `value` and `onChange` props.
               Inputs render what they're told - their current value comes from app state.
               At each keystroke, a change handler fires to change app state. */}
+              <input 
+                type=
+              />
         </label>
 
         <label>Email

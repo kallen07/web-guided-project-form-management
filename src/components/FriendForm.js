9b3d53f546a578e081d4d42c74109708ef1a8203
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-26 12:23:39 -0600

    wip

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -1,56 +1,59 @@
-import React from 'react'
+import React from "react";
 
 export default function FriendForm(props) {
   // THESE ARE THE **EXACT PROPS** FriendForm EXPECTS!!!
-  const { values, update, submit } = props
+  const { values, update, submit } = props;
 
-  const onChange = evt => {
+  const onChange = (evt) => {
     // 🔥 STEP 6 - IMPLEMENT the change handler for our inputs and dropdown
     // a) pull the name of the input from the event object
     // b) pull the value of the input from the event object
     // c) use the `update` callback coming in through props
-  }
+  };
 
-  const onSubmit = evt => {
+  const onSubmit = (evt) => {
     // 🔥 STEP 7 - IMPLEMENT the submit handler
     // a) don't allow the browser to reload!
     // c) use the `submit` callback coming in through props
-  }
+  };
 
   return (
-    <form className='form container' onSubmit={onSubmit}>
-      <div className='form-group inputs'>
+    <form className="form container" onSubmit={onSubmit}>
+      <div className="form-group inputs">
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
-        <label>Username
+        <label>
+          Username
           {/* 🔥 STEP 3 - Make an input of type `text` for username.
               Controlled inputs need `value` and `onChange` props.
               Inputs render what they're told - their current value comes from app state.
               At each keystroke, a change handler fires to change app state. */}
-              <input 
-                type='text'
-                name='username'
-                onChange={onChange}
-                value={values.user}
-              />
+          <input
+            type="text"
+            name="username"
+            onChange={onChange}
+            value={values.username}
+          />
         </label>
 
-        <label>Email
+        <label>
+          Email
           {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
         </label>
 
         {/* ////////// DROPDOWN ////////// */}
         {/* ////////// DROPDOWN ////////// */}
         {/* ////////// DROPDOWN ////////// */}
-        <label>Role
+        <label>
+          Role
           {/* 🔥 STEP 5 - Make dropdown for role. */}
         </label>
 
-        <div className='submit'>
+        <div className="submit">
           <button>submit</button>
         </div>
       </div>
     </form>
-  )
+  );
 }

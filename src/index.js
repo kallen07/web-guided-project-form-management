39ff024b99f885a90e408768111a721c28bd2316
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-26 11:38:53 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function SimpleForm() {
   const [formValues, setFormValues] = useState(initialFormValues);
 
   const change = (evt) => {
-    debugger;
+    
   };
   const submit = (evt) => {};
 

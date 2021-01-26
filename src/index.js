17170bcb47c1e6966fadd6a6d965c4b539c6a834
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-26 11:59:45 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ function SimpleForm() {
     // it will override whatever the value of name was in formValues
   };
   const submit = (evt) => {
+    evt.preventDefault();
     const newPet = {
       petName: formValues.petName.trim(),
       petType: formValues.petType.trim(),

4ade1e3cdd641a008a541019c9560d77ff7c4553
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2021-01-26 11:18:18 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 // 👉 App contains a more sophisticated form we'll flesh out later
-import App from './components/App'
+import App from "./components/App";
 
 // 👉 First let's build a SimpleForm to add more pets:
 const petsList = [
-  { petName: 'Fido', petType: 'dog' },
-  { petName: 'Tweetie', petType: 'canary' },
-  { petName: 'Goldie', petType: 'fish' },
-]
+  { petName: "Fido", petType: "dog" },
+  { petName: "Tweetie", petType: "canary" },
+  { petName: "Goldie", petType: "fish" },
+];
 
 function SimpleForm() {
-  return <div>Ready to start GP!</div>
+  return <div>Ready to start GP!</div>;
 }
 
 render(
   <>
     <SimpleForm />
     {/* <App /> */}
-  </>
-  , document.querySelector('#root')
-)
+  </>,
+  document.querySelector("#root")
+);
